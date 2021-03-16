be77f4335a141b766fad695ec65c1fe42e90e08b
gfp34 <gfpaiement@gmail.com>
2021-03-16 14:31:56 -0400

    Implemented saving brackets to csv

--- march_madness.py
+++ march_madness.py
@@ -1,5 +1,6 @@
-import random
 import csv
+import random
+
 
 CHALK = 'Chalk'
 SIMULATED = 'Simulated'
@@ -43,21 +44,21 @@ PLAYIN_INDEX = {
 def main():
     teams = read_teams_file("data/fivethirtyeight_ncaa_forecasts.csv")
     b = Bracket(teams, SIMULATED)
-    print(str(b), '\n')
     b.play()
-    print(str(b))
+    print(b)
+    b.save("random_bracket.csv")
 
 
 class Bracket:
 
     def __init__(self, teams, picker):
+        self.teams = teams
         self.bracket_heap = [Game(picker) for _ in range(NUM_GAMES)]
         for team in teams:
             if team.playin_flag:
                 self.bracket_heap[PLAYIN_INDEX[team.region][team.seed]].add_team(team)
             else:
                 self.bracket_heap[BRACKET_INDEX[team.seed] + REGION_OFFSET[team.region]].add_team(team)
-        self.winner = None
 
     def play(self):
         # Play the first four
@@ -66,13 +67,30 @@ class Bracket:
             self.bracket_heap[BRACKET_INDEX[winner.seed] + REGION_OFFSET[winner.region]].add_team(winner)
 
         def play_game(i):
-            # TODO
             if not(self.bracket_heap[i].is_ready()):
                 self.bracket_heap[i].add_team(play_game(2 * i + 1))
                 self.bracket_heap[i].add_team(play_game(2 * i + 2))
             return self.bracket_heap[i].pick_winner()
+        play_game(0)
 
-        self.winner = play_game(0)
+    def save(self, filename):
+        bracket_csv = open(filename, 'w')
+        bracket_writer = csv.writer(bracket_csv, delimiter=',')
+        bracket_writer.writerow(["team_name",  "team_seed", "team_rating", "team_region", "playin_flag", "team_id"
+                                 "rd1_win", "rd2_win", "rd3_win", "rd4_win", "rd5_win", "rd6_win", "rd7_win"])
+        bracket_sections = {1: 63, 2: 31, 3: 15, 4: 7, 5: 3, 6: 1, 7: 0}
+        for team in self.teams:
+            team_data = [team.name, team.seed, team.rating, team.region, int(team.playin_flag)]
+            wins = []
+            for i in bracket_sections:
+                if i == 1 and not team.playin_flag:
+                    wins += [1]
+                else:
+                    start, end = bracket_sections[i], len(self.bracket_heap) if i == 1 else bracket_sections[i-1]
+                    # Check if team is in this rounds section of game winners
+                    wins += [int(team in [g.winner for g in self.bracket_heap[start:end]])]
+            bracket_writer.writerow(team_data + wins)
+        bracket_csv.close()
 
     def __str__(self):
         s = ""
@@ -100,10 +118,10 @@ class Bracket:
                 s += add_round(list(rounds)[i], list(rounds)[i-1])
             s += '\n'
 
-        if self.winner is None:
+        if self.bracket_heap[0].winner is None:
             s += "WINNER: TBD"
         else:
-            s += "WINNER: " + str(self.winner)
+            s += "WINNER: " + str(self.bracket_heap[0].winner)
 
         return s
 
@@ -114,6 +132,7 @@ class Game:
         self.picker = picker
         self.teams = []
         self.win_prob = None
+        self.winner = None
 
     def add_team(self, team):
         self.teams += [team]
@@ -143,9 +162,10 @@ class Game:
 
     def pick_winner(self):
         if self.picker == SIMULATED:
-            return self.random_winner()
+            self.winner = self.random_winner()
         else:
-            return self.chalk_winner()
+            self.winner = self.chalk_winner()
+        return self.winner
 
     def __str__(self):
         if len(self.teams) == 0:
@@ -172,6 +192,9 @@ class Team:
     def __str__(self):
         return f"({self.seed}){self.name}"
 
+    def __eq__(self, other):
+        return type(other) == Team and self.team_id == other.team_id
+
 
 def read_teams_file(filename):
     try:
@@ -183,6 +206,7 @@ def read_teams_file(filename):
             if team["gender"] == "mens":
                 teams += [Team(team["team_name"], float(team["team_rating"]), int(team["team_seed"][:2]),
                                team["team_region"], bool(int(team["playin_flag"])), int(team["team_id"]))]
+        csv_file.close()
         return teams
     except FileExistsError:
         err_message = "Error: " + filename + " does not exist"
